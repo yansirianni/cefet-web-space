f246f6c52f2e19b5ff76cec1120b67a1afa1ce97
Yan Victor <yanvictorsa@yahoo.com.br>
2021-06-20 10:52:32 -0300

    Expand/collapse paragraphs - Exercice 3

--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,26 @@
 // Faça o exercício dos PARÁGRAFOS aqui
-// Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+const botoesExpandirRetrair = document.querySelectorAll('.botao-expandir-retrair');
+
+for (let botaoExpandirRetrairEl of botoesExpandirRetrair){
+    botaoExpandirRetrairEl.addEventListener('click', expandirRetrair);
+}
+
+function expandirRetrair(e){
+    const botaoEl = e.currentTarget;
+    const pEl = botaoEl.parentNode;
+    
+    botaoEl.innerHTML = alteraConteudoDoBotao(botaoEl);
+    pEl.classList.toggle('expandido');    
+}
+
+function alteraConteudoDoBotao(el){
+    if(el.innerHTML === '+'){
+        return '-';
+    }
+    else if(el.innerHTML === '-'){
+        return '+';
+    }
+    
+    return el.innerHTML;
+}
